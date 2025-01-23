72ce4380b44f842f8f6b1d47268b9a1b204875eb
AliKhachab <a.khachab620@gmail.com>
2025-01-22 23:42:12 -0500

    pushing for save

--- src/components/Sorting.js
+++ src/components/Sorting.js
@@ -5,6 +5,20 @@ import {
 } from '../common.js';
 
 const clickHandler = event => {
-    event.preventDefault();
+    const clickedButtonEl = event.target.closest('.sorting__button');
+
+    if (!clickedButtonEl) {
+        console.log('not a button');
+        return;
+    }; // stop the function if the thing that was clicked was the area around the button
+    // and not the button itself
+    
+    const recent =  clickedButtonEl.className.includes('--recent') ? true : false;
+    
+    if (recent) {
+        console.log('recent');
+    } else {
+        console.log('relevant');
+    }
 };
 sortingEl.addEventListener('click', clickHandler);
